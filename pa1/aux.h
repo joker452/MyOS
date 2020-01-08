c7d19375048c42aefd832136aac39179416418a8
Bowen Deng <cs120wjp@ieng9.ucsd.edu>
2020-01-08 10:43:47 -0800

    initial commit

---
+++ pa1/aux.h
@@ -0,0 +1,9 @@
+/* Copyright 1989-2020, Joseph Pasquale, University of California, San Diego
+ *
+ *	UMIX auxiliary function interfaces
+ */
+
+void Printf(char *fmt, ...);
+void DPrintf(char *fmt, ...);
+void SlowPrintf(int n, char *fmt, ...);
+int Delay(int t);
